6950e5752d1d995ac3e93fd6be6f6f668af59527
Gotar <eltahir.g@gmail.com>
2020-09-18 02:11:48 -0500

    Final code

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
 
-# create an engine and connection setup..reflect ...
+# create an engine and connection setup..reflect tables into sqlalchemy ORM ...
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-# re-define our tables to python...
+# re-define our tables to python and save them...
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
@@ -32,10 +32,10 @@ def welcome():
         f"/api/v1.0/start<br/>"
         f"/api/v1.0/start/end"
     )
-
+# create a route..
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    ## create our session
+    ## create session
     session = Session(engine)
 
     # query database for needed information
@@ -72,7 +72,7 @@ def station():
 def tobs():
     session = Session(engine)
 
-    """ Return a json list of temperature observation (tobs) of the most active ststion for the last year"""
+    """ Return a json list of temperature observation (tobs) of the most active station for the last year"""
     """ Most active station is station number USC00519281..this is from the first part of the hw"""
 
     tobs_result = session.query(Measurement.tobs).\
@@ -86,45 +86,39 @@ def tobs():
         tobs_list.append(tob)
     return jsonify(tobs_list)
 
-@app.route('/api/v1.0/start')
-def start():
+@app.route('/api/v1.0/<start>')
+def stt(start=None):
     session = Session(engine)
 
     """ Return a json list of the minimum, maximum and average temperature for a given start date"""
-    """Prompt user to input a start date for his trip"""
-    """Date entry format is: YY-MM-DD"""
-
-    start_date = input('Please Input your Start Date: ')  
+    """Start date is now a parameter in the API get request """
+    """Date entry format is: %Y-%m-%d"""  
 
      # query database for needed information
     sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
-    start_results = session.query(*sel).\
-        filter(Measurement.date >= start_date).all()
+    start_result = session.query(*sel).\
+        filter(Measurement.date >= start).all()
     session.close()
 
     # create the list and then iterate over our query to return needed values....
     start_list = []
-    for res in start_results:
+    for res in start_result:
         start_list.append(res)
     return jsonify(start_list)
 
 
-@app.route('/api/v1.0/startend')
-def startend():
+@app.route('/api/v1.0/<start>/<end>')
+def st(start=None, end=None):
     session = Session(engine)
 
     """ Return a json list of the minimum, maximum and average temperature for a given start-end date range"""
-    """Prompt user to input a start and end date for his trip, Date entry format is: YY-MM-DD"""
-
-    # Date entry format is: YY-MM-DD 
-    start_date = input('Please Input your Start Date: ') 
-    end_date =  input('Please Input your End Date: ')  
-
+    """Date entry format is: %Y-%m-%d"""
+    
      # query database for needed information
     sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
     start_results = session.query(*sel).\
-        filter(Measurement.date >= start_date).\
-        filter(Measurement.date <= end_date).all()
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
     session.close()
 
     # create the list and then iterate over our query to return needed values....
